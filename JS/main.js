09b3b391297b7821e440b3b5ea55ef9ba2e17758
FrancescoPepi <francescopepi16@gmail.com>
2023-08-25 18:54:07 +0200

    errore

--- JS/main.js
+++ JS/main.js
@@ -26,3 +26,33 @@ const images = [
     text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
   },
 ];
+
+// RECUPERARE DOM
+const next = document.getElementById("next");
+const back = document.getElementById("back");
+const container = document.getElementById("container-img");
+
+// const generateImgBox = images.forEach((image) => {
+//   console.log(image.image);
+//   creationImgBox(image.image);
+// });
+
+// function creationImgBox(image) {
+//   prova.innerHTML += `
+//   <div class="box-img">
+//             <img src="./IMG/${image}" alt="" />
+//           </div>
+//   `;
+// }
+// for (let i = 0; i < images.length; i++) {
+//   creationImgBox(image.image);
+// }
+
+for (const member of images) {
+  container.innerHTML += `
+     <div class="box-img">
+               <img src="./IMG/${member}" alt="" />
+             </div>
+     `;
+  console.log(member);
+}
